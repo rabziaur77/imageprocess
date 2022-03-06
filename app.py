749cb0bb83590f0fb90dc261b0cfe266fa126f6e
Ziaur Rab <rabziaur77@outlook.com>
2022-03-06 21:43:44 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import cv2 as cv
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    img = cv.imread("img.png")
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     return "Hello World!"
 
 
